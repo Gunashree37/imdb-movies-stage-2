3ce523651aa439f009373bfd31af1e4c7151f9d4
Gunashree S <gunashrees341@gmail.com>
2021-12-05 18:05:20 +0530

    Update Recommendation.js

--- screens/Recommendation.js
+++ screens/Recommendation.js
@@ -33,34 +33,14 @@ export default class RecommendedMoviesScreen extends Component {
       });
   };
 
-  keyExtractor = (item, index) => index.toString();
-
-  renderItems = ({ item, index }) => {
-    return (
-      <Card
-        key={`card-${index}`}
-        image={{ uri: item.poster_link }}
-        imageProps={{ resizeMode: "cover" }}
-        featuredTitle={item.title}
-        containerStyle={styles.cardContainer}
-        featuredTitleStyle={styles.title}
-        featuredSubtitle={`${
-          item.release_date.split("-")[0]
-        } | ${this.timeConvert(item.duration)}`}
-        featuredSubtitleStyle={styles.subtitle}
-      ></Card>
-    );
-  };
-
+  
+  
   render() {
     const { data } = this.state;
     return (
       <View style={styles.container}>
-        <FlatList
-          data={data}
-          keyExtractor={this.keyExtractor}
-          renderItem={this.renderItems}
-        />
+        
+      
       </View>
     );
   }
